9c9002b8ee3e68d6490973fdece7589c0f834796
Riqho <riqhofahturrahman124@gmail.com>
2024-03-21 22:19:58 +0700

    first commit

---
+++ src/component/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <h1>Hero</h1>
+      <p>isinyaaa</p>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../component/Navbar";
+import Footer from "../component/Footer";
+
+const About = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>About</h1>
+      <p>halaman about.</p>
+      <Footer />
+    </div>
+  );
+};
+
+export default About;
